d37ea1e03aa100af1a266782293e6fef2e15bce9
Victor Henriksson <vhenrixon@gmail.com>
2020-02-02 21:26:36 -0500

    PS1 WPI

--- Problem Set 1/ps1.py
+++ Problem Set 1/ps1.py
@@ -6,12 +6,12 @@ myString = "Hi"
 myInteger = 1
 myDouble = 3.3141
 
-# How print out a value 
+# How to print out a value 
 print(myString)
 # How to print multipal things 
 print(myString + str(myInteger))
 
-#Change a variable type
+#Changing a variables type
 str(myDouble) # This will give you "1"
 int(myDouble) # --> 3 and will get rid of everything after the decimal is gone 
 
